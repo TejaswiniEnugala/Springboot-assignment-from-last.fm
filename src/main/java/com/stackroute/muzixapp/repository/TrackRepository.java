07e2de9f46f64da94d6bfe6f0221f7038bf22756
TejaswiniEnugala <tejaswinienugala.in@gmail.com>
2019-07-30 11:40:38 +0530

    assignment completed

---
+++ src/main/java/com/stackroute/muzixapp/repository/TrackRepository.java
@@ -0,0 +1,13 @@
+package com.stackroute.muzixapp.repository;
+
+import com.stackroute.muzixapp.domain.Track;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface TrackRepository extends CrudRepository<Track,Integer> {
+
+    @Query("SELECT t FROM Track t WHERE t.name = ?1")
+    List<Track> getTrackbyName(String name);
+}
